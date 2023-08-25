5aea1e8cad3482894c5c8c3714f922718b89e43f
hbc <bcxxxxxx@gmail.com>
2023-08-26 01:21:18 +0800

    feat: add proof http server

---
+++ cmd/proof/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/alecthomas/kong"
+)
+
+type CLI struct {
+	ServerAddr string `cmd:"server-addr" help:"the address to listen on" default:"127.0.0.1:8888"`
+	Scenario   string `cmd:"scenario" enum:"none,proof" help:"the scenario to run" default:"none"`
+
+	ServerReadHeaderTimeout time.Duration `cmd:"server-read-header-timeout" help:"the amount of time allowed to read request headers. Non-positive value means no timeout"`
+	ServerReadTimeout       time.Duration `cmd:"server-read-timeout" help:"the maximum duration for reading the entire request, including the body. Non-positive value means no timeout"`
+	ServerWriteTimeout      time.Duration `cmd:"server-write-timeout" help:"the maximum duration before timing out writes of the response. Non-positive value means no timeout"`
+}
+
+func (c *CLI) defaults() error {
+	switch c.Scenario {
+	case "proof":
+		if c.ServerReadHeaderTimeout <= 0 {
+			c.ServerReadHeaderTimeout = 3 * time.Second
+		}
+		if c.ServerReadTimeout <= 0 {
+			c.ServerReadTimeout = 60 * time.Second
+		}
+		if c.ServerWriteTimeout <= 0 {
+			c.ServerWriteTimeout = 60 * time.Second
+		}
+	default:
+		if c.ServerReadHeaderTimeout <= 0 {
+			c.ServerReadHeaderTimeout = 0
+		}
+		if c.ServerReadTimeout <= 0 {
+			c.ServerReadTimeout = 0
+		}
+		if c.ServerWriteTimeout <= 0 {
+			c.ServerWriteTimeout = 0
+		}
+	}
+
+	return nil
+}
+
+func (c *CLI) CreateServer() *http.Server {
+	return &http.Server{
+		Addr:              c.ServerAddr,
+		ReadHeaderTimeout: c.ServerReadHeaderTimeout,
+		ReadTimeout:       c.ServerReadTimeout,
+		WriteTimeout:      c.ServerWriteTimeout,
+
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("here")
+
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte("ok"))
+		}),
+	}
+}
+
+func main() {
+	cli := &CLI{}
+	cliCtx := kong.Parse(cli)
+
+	if err := cli.defaults(); err != nil {
+		cliCtx.FatalIfErrorf(err)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	server := cli.CreateServer()
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
+			cliCtx.FatalIfErrorf(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	server.Shutdown(shutdownCtx)
+}
