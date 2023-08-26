6aa7b24ac790202c3cd23ecdfd9f67b13146c569
hbc <bcxxxxxx@gmail.com>
2023-08-26 13:16:47 +0800

    test: add single connection test for easy debugging

--- slowloris_test.go
+++ slowloris_test.go
@@ -19,6 +19,44 @@ func Test_Slowloris_defaults(t *testing.T) {
 	assert.NotNil(t, v.randn)
 }
 
+func Test_Slowloris_single(t *testing.T) {
+	t.Parallel()
+
+	counter := testhttp.NewConnStateCounters()
+
+	testCtx, stopTest := getTestContext()
+	defer stopTest()
+
+	serverUrl, serverStopped := testhttp.CreateTestServer(testCtx, t, func(s *http.Server) {
+		s.ReadHeaderTimeout = 300 * time.Millisecond
+		s.ConnState = counter.ServerConnState
+	})
+
+	target := getTestTarget(t, *serverUrl)
+	target.Connections = 1
+	attack := Slowloris{
+		Target: target,
+		SendGibberish: true,
+		GibberishInterval: 10 * time.Millisecond,
+	}
+
+	assert.NoError(t, attack.Run(testCtx))
+	stopTest()
+	<-serverStopped
+
+	t.Log(counter.String())
+
+	conns := counter.GetConns()
+	assert.NotEmpty(t, conns, "should have at least one connection")
+	assert.Greater(t, len(conns), target.Connections, "should create more connections")
+	for _, conn := range conns {
+		timeline := counter.GetConnStateTimeline(conn)
+		assert.GreaterOrEqual(t, len(timeline), 1, "should have at least one state")
+		// a typical state transition is: new - (due to server close) -> active -> closed
+		assert.Equal(t, http.StateNew, timeline[0])
+	}
+}
+
 func Test_Slowloris_vulnerable(t *testing.T) {
 	t.Parallel()
 
